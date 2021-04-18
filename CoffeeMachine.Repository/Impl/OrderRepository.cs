cd5b8a451fdab85c28a74e074960b22e90e5f63e
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-04-18 04:16:10 -0700

    Add IOrderRepository and Impl

---
+++ CoffeeMachine.Repository/Impl/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeMachine.Repository.Models;
+
+namespace CoffeeMachine.Repository.Impl
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(CoffeeMachineDbContext dbContext)
+            : base(dbContext)
+        { }
+    }
+}
