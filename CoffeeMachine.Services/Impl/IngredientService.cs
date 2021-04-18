8d5bd7c850e685b724032d45ea0fc5e0d40d24c8
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-04-18 04:30:51 -0700

    Add IIngredientService and Impl

---
+++ CoffeeMachine.Services/Impl/IngredientService.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeMachine.Repository;
+using CoffeeMachine.Repository.Models;
+
+namespace CoffeeMachine.Services.Impl
+{
+    public class IngredientService : BaseService<Ingredient>, IIngredientService
+    {
+        public IngredientService(IBaseRepository<Ingredient> repo)
+            : base(repo)
+        { }
+    }
+}
