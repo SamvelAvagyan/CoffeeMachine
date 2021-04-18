5a0a931bf6f671e189f2fc5bbbf32e80ef2ca941
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-04-18 04:28:13 -0700

    Add IIngredientRepository and Impl

--- CoffeeMachine.Repository/CoffeeMachineDbContext.cs
+++ CoffeeMachine.Repository/CoffeeMachineDbContext.cs
@@ -11,6 +11,7 @@ namespace CoffeeMachine.Repository
 
         public DbSet<Coffee> Coffees { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ CoffeeMachine.Repository/Impl/IngredientRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeMachine.Repository.Models;
+
+namespace CoffeeMachine.Repository.Impl
+{
+    public class IngredientRepository : BaseRepository<Ingredient>, IIngredientRepository
+    {
+        public IngredientRepository(CoffeeMachineDbContext dbContext)
+            : base(dbContext)
+        { }
+    }
+}
