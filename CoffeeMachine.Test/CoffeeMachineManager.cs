ac9ec53927f94fb802bca867023a54273337288c
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-04-20 11:46:02 -0700

    Add User Selection and Coin Insertion

--- CoffeeMachine.Test/CoffeeMachineManager.cs
+++ CoffeeMachine.Test/CoffeeMachineManager.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using CoffeeMachine.Repository;
+using CoffeeMachine.Repository.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CoffeeMachine.Test
 {
     public static class CoffeeMachineManager
     {
+        public static IUserRepository userRepo = Program.serviceProvider.GetService<IUserRepository>();
+        public static ICoffeeRepository coffeeRepo = Program.serviceProvider.GetService<ICoffeeRepository>();
+        private static Order order;
+        private static User user;
+
         public static void SelectUser()
         {
             Console.Write("Please, insert id of user, who wants to buy a coffee: ");
@@ -13,16 +21,54 @@ namespace CoffeeMachine.Test
             {
                 int.TryParse(Console.ReadLine(), out id);
 
-                if(id == 0)
+                if (id == 0 || userRepo.GetById(id) == null)
                 {
                     Console.Write("Please, insert correct id: ");
                 }
-            } while (id == 0);      
+            } while (id == 0 || userRepo.GetById(id) == null);
+
+            order = new Order();
+            user = userRepo.GetById(id);
+            order.User = user;
+            order.UserId = user.Id;
+
+            Console.WriteLine($"{user.Name}, {order.User.Name}, {order.UserId}");
         }
 
         public static void InsertCoins()
         {
+            Console.Write("Please, insert coin (50, 100, 200, 500) (Type enter to finish this action): ");
+            string c;
+            int coin;
+            int sum = 0;
+            bool t;
 
+            do
+            {
+
+                c = Console.ReadLine();
+                if (c == "")
+                {
+                    break;
+                }
+
+                int.TryParse(c, out coin);
+                t = coin != 50 && coin != 100 && coin != 200 && coin != 500;
+
+                if (coin == 0 || t)
+                {
+                    coin = 0;
+                    Console.Write("Please, insert coins that allowed (50, 100, 200, 500) (Type enter to finish this action): ");
+                }
+                else
+                {
+                    Console.Write("Please, insert coin (50, 100, 200, 500) (Type enter to finish this action): ");
+                }
+
+                sum += coin;
+            } while (true);
+
+            Console.WriteLine(sum);
         }
     }
 }
--- CoffeeMachine.Test/Program.cs
+++ CoffeeMachine.Test/Program.cs
@@ -1,8 +1,6 @@
 ﻿using CoffeeMachine.Repository;
 using CoffeeMachine.Repository.Impl;
 using CoffeeMachine.Repository.Models;
-using CoffeeMachine.Services;
-using CoffeeMachine.Services.Impl;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -11,43 +9,59 @@ namespace CoffeeMachine.Test
 {
     class Program
     {
-        private static IServiceProvider serviceProvider;
-        private static ICoffeeService coffeeService;
+        public static IServiceProvider serviceProvider;
 
         static void Main(string[] args)
         {
             Configure();
-            //CoffeeMachineManager.SelectUser();
-            CreateCoffees(10);
+            //CreateCoffees(10);
+            //CreateUsers(3);
+
+            CoffeeMachineManager.SelectUser();
+            CoffeeMachineManager.InsertCoins();
         }
 
         private static void Configure()
         {
             var services = new ServiceCollection();
-            //services.AddDbContext(DataOptions.ConnectionString);
+
             services.AddDbContext<CoffeeMachineDbContext>(con => con.UseSqlite(DataOptions.ConnectionString));
-            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
-            services.AddScoped<ICoffeeService, CoffeeService>();
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<ICoffeeRepository, CoffeeRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IIngredientRepository, IngredientRepository>();
 
             serviceProvider = services.BuildServiceProvider();
         }
 
         private static void CreateCoffees(int count)
         {
-            coffeeService = serviceProvider.GetService<ICoffeeService>();
             var rnd = new Random();
 
             for (int i = 0; i < count; i++)
             {
-                coffeeService.Add(new Coffee()
+                CoffeeMachineManager.coffeeRepo.Add(new Coffee()
                 {
                     Name = $"Coffee{i + 1}",
                     CoffeePortion = Math.Round(rnd.NextDouble(), 2),
-                    Price = rnd.Next(1, 5) * 10,
+                    Price = rnd.Next(1, 5) * 100,
                     SugarPortion = Math.Round(rnd.NextDouble(), 2),
                     WaterPortion = Math.Round(rnd.NextDouble(), 2)
+                });
+            }
+        }
+
+        private static void CreateUsers(int count)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                CoffeeMachineManager.userRepo.Add(new User()
+                {
+                    Name = $"Coffee{i + 1}",
+                    Balance = rnd.Next(2, 5) * 100
                 });
             }
         }
